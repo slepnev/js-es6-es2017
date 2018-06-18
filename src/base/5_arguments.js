23ccc00890c6ad077b9db1b6cc76480fa904952a
alexander <a.slepnev@it365.su>
2018-06-18 13:38:54 +0300

    Test Mocha

---
+++ src/base/5_arguments.js
@@ -0,0 +1,20 @@
+/*
+  Arguments - праметры в функции.
+  arguments - это не массив, можно только arguments[i] и arguments.length
+*/
+function sayHi() {
+  for (let i = 0; i < arguments.length; i++) {
+    console.log( "Привет, " + arguments[i] );
+  }
+}
+sayHi("Винни", "Пятачок"); // 'Привет, Винни', 'Привет, Пятачок'
+
+/*
+  Аргументы по умолчанию через ||
+*/
+function showWarning(width, height, title, contents) {
+  width = width || 200; // если не указана width, то width = 200
+  height = height || 100; // если нет height, то height = 100
+  title = title || "Предупреждение";
+  //...
+}
