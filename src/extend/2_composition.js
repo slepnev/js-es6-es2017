693f830f2e0e9611e589945231b0b6597bb07037
alexander <a.slepnev@it365.su>
2018-07-05 17:57:23 +0300

    Extend

--- src/extend/2_composition.js
+++ src/extend/2_composition.js
@@ -1,14 +1,29 @@
 /*
-  1.
+  1. Композиция - это комбинирование двух или более функции чтобы создать новую функцию
 */
 
-const toSlug = input => {
-  const words = input.split(' ');
-  const lowercaseWords = words.map(word => word.toLowerCase());
-  const slug = lowercaseWords.join('-');
-  const encodedSlug = encodeURIComponent(slug);
+// Все функции карированы:
+const toSlug = input =>
+  R.map(R.toLowerCase())(
+    R.split(' ')(input)
+  );
 
-  return encodedSlug;
-};
+
+// Без переменных:
+// const toSlug = input => encodeURIComponent(
+//   input.split(' ')
+//     .map(str => str.toLowerCase())
+//     .join('-')
+// );
+
+// Через переменные:
+// const toSlug = input => {
+//   const words = input.split(' ');
+//   const lowercaseWords = words.map(word => word.toLowerCase());
+//   const slug = lowercaseWords.join('-');
+//   const encodedSlug = encodeURIComponent(slug);
+//
+//   return encodedSlug;
+// };
 
 const slug = toSlug('This is composition');
